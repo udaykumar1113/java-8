1d11eb0088a7ddd63f6b7a9f09c18627afbcef6f
udayk <udaykumar1113@gmail.com>
2018-12-28 19:09:25 -0500

    Date and Time using Joda API

---
+++ src/main/java/com/uday/java8/joda/LocalDateTime_39.java
@@ -0,0 +1,14 @@
+package com.uday.java8.joda;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class LocalDateTime_39 {
+    public static void main(String args[]){
+        LocalDate localDate=LocalDate.now();
+        LocalTime localTime=LocalTime.now();
+
+        System.out.println(localDate+" "+localTime);
+    }
+
+}
